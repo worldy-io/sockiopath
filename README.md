49be331baa1360cfb45ce71050c4268e70bad62d
AnthonyRuffino <aruffino84@gmail.com>
2022-10-14 14:02:50 +0200

    pre-commit hooks

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sockiopath
 A UDP and WebSocket server
+
+
+Set up the pre-commit hook
+```shell script
+ln -s ../../pre-commit.sh .git/hooks/pre-commit
+```
---
+++ pre-commit.sh
@@ -0,0 +1,10 @@
+STASH_NAME="pre-commit-$(date +%s)"
+git stash save --quiet --keep-index --include-untracked $STASH_NAME
+
+STASHES=$(git stash list)
+if [[ $STASHES == *"$STASH_NAME" ]]; then
+  git stash pop --quiet
+fi
+
+./mvnw clean
+./mvnw verify
